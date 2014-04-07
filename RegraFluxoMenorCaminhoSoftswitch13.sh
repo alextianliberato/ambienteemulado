e15637388522431457ed144c52086bbea563ea7e
Alextian B Liberato <abliberato@gmail.com>
2014-04-07 15:02:25 -0300

    Create RegraFluxoMenorCaminhoSoftswitch13.sh
    Regra de Fluxo com menor caminho no Softswitch13.

---
+++ RegraFluxoMenorCaminhoSoftswitch13.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#regras do s1 ida e volta
+dpctl tcp:127.0.0.1:6634 flow-mod table=0,cmd=add eth_type=0x806,arp_tpa=10.0.0.1 apply:output=1
+dpctl tcp:127.0.0.1:6634 flow-mod table=0,cmd=add eth_type=0x806,arp_tpa=10.0.0.2 apply:output=3
+dpctl tcp:127.0.0.1:6634 flow-mod cmd=add,table=0 eth_type=0x800,ip_dst=10.0.0.2 apply:output=3
+dpctl tcp:127.0.0.1:6634 flow-mod table=0,cmd=add in_port=3 apply:output=1
+
+#regras do s4 ida e volta..
+dpctl tcp:127.0.0.1:6635 flow-mod table=0,cmd=add eth_type=0x806,arp_tpa=10.0.0.1 apply:output=1
+dpctl tcp:127.0.0.1:6635 flow-mod table=0,cmd=add eth_type=0x806,arp_tpa=10.0.0.2 apply:output=3
+dpctl tcp:127.0.0.1:6635 flow-mod table=0,cmd=add eth_type=0x800,ip_dst=10.0.0.1 apply:output=1
+dpctl tcp:127.0.0.1:6635 flow-mod table=0,cmd=add eth_type=0x800,ip_dst=10.0.0.2 apply:output=3
+
